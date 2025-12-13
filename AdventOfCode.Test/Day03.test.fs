306654d59cd7e33ae70a0e4f28c216a4fb0fb23f
Theodor Heiselberg <theodor.heiselberg@stil.dk>
2025-12-13 20:14:53 +0100

    Use int64 Day03.02 answered

--- AdventOfCode.Test/Day03.test.fs
+++ AdventOfCode.Test/Day03.test.fs
@@ -6,19 +6,59 @@ open AdventOfCode.Lobby
 [<Fact>]
 let ``Answer from extended test data`` () =
     let answer = maxJoltageUsingNBatteries "TestData/day03.01.extended-test.txt" 2
-    Assert.Equal(478, answer)
+    Assert.Equal(478L, answer)
 
 [<Fact>]
 let ``Answer from test data`` () =
     let answer = maxJoltageUsingNBatteries "TestData/day03.01.test.txt" 2
-    Assert.Equal(357, answer)
+    Assert.Equal(357L, answer)
 
 [<Fact>]
 let ``Answer for level 1`` () =
     let answer = maxJoltageUsingNBatteries "TestData/day03.01.txt" 2
-    Assert.Equal(17403, answer)
+    Assert.Equal(17403L, answer)
+
+[<Theory>]
+[<InlineData("987654321111111",98L)>]
+[<InlineData("811111111111119",89L)>]
+[<InlineData("234234234234278",78L)>]
+[<InlineData("818181911112111",92L)>]
+[<InlineData("111113311111111",33L)>]
+[<InlineData("441113311111144",44L)>]
+[<InlineData("111113311111155",55L)>]
+let ``Answer for level 2 using 2 batteries`` (input: string, expected: int64) =
+    let v = input
+            |> strArr2NumArr
+            |> getVal 2
+
+    Assert.Equal(expected=expected, actual = v)
 
 [<Fact>]
-let ``Answer for level 2 using 2 batteries`` () =
-    let answer = maxJoltageUsingNBatteries "TestData/day03.01.txt" 2
-    Assert.Equal(478, answer)
+let ``Answer using 3 batteries`` () =
+    let batteries = "1987654321111111"
+
+    let v = batteries
+            |> strArr2NumArr
+            |> getVal 3
+
+    Assert.Equal(expected=987L, actual = v)
+
+[<Fact>]
+let ``Answer using 4 batteries`` () =
+    let batteries = "1987654321111111"
+
+    let v = batteries
+            |> strArr2NumArr
+            |> getVal 4
+
+    Assert.Equal(expected=9876L, actual = v)
+
+[<Fact>]
+let ``Answer using 12 batteries`` () =
+    let batteries = "66655456766266656565654545666566746526656565329434656545665565566665566654446966665656769876543219871"
+
+    let v = batteries
+            |> strArr2NumArr
+            |> getVal 12
+    let ciffersInV = v |> abs |> string |> String.length
+    Assert.Equal(expected=12, actual = ciffersInV)
--- AdventOfCode/Day03.fs
+++ AdventOfCode/Day03.fs
@@ -2,41 +2,52 @@ namespace AdventOfCode
 
 open AdventOfCode.FileHandler
 module Lobby =
-    let strArr2NumArr (strArr: string) : int array = strArr |> Array.ofSeq |> Array.map(fun x -> int x - int '0')
+    let strArr2NumArr (strArr: string) : int64 array = strArr |> Array.ofSeq |> Array.map(fun x -> int64 x - int64 '0')
 
-    type iterItem = { index: int; value: int }
+    type iterItem = { index: int; value: int64 }
     let maxWithIndex (prev: iterItem ) (next: iterItem) =
         if prev.value >= next.value then
             prev
         else
             next
     let emptyIterItem = { index = -1; value = -1 }
+    let toIterItems (l: int64 array) : iterItem array =
+            l |> Array.mapi(fun i y ->  { index = i; value = y })
+    let toIntArr (l: string array) = l |> Array.map(fun x -> strArr2NumArr x)
+    let ciffers2Multiplier (ciffers: int) : int64=
+        System.Math.Pow(10.0, float ciffers - 1.0) |> int64
 
-    let ciffers2Multiplier (ciffers: int) =
-        System.Math.Pow(10.0, float ciffers - 1.0)
-    let rec toVal (list: iterItem array) (ciffers: int): int =
+    let rec getVal (ciffers: int) (l: int64 array): int64 =
+        let list = l |> toIterItems
+        if ciffers = 0 then
+            0
+        else if ciffers = 1 then
+            let o =list |> Array.fold (fun prev next -> maxWithIndex prev next) emptyIterItem
+            o.value
+        else
+        let m = ciffers |> ciffers2Multiplier
         let noTailList = list.[0..list.Length-ciffers]
-        let tempMax = noTailList |>  Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
-        let tail = list.[list.Length-1]
-        if tempMax.value = tail.value then
-            tempMax.value * 10 + tail.value // Return fast!
+        let tempMax = noTailList
+                    |>  Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
+        let tail = list.[list.Length-ciffers+1]
+        let tailVal = getVal (ciffers-1) l.[tempMax.index + 1 .. l.Length]
+        if tempMax.value * (m |> int64) = tail.value then
+            tempMax.value * (m |> int64) + tail.value + getVal (ciffers-1) l.[tempMax.index + 2 ..list.Length]
         else
         let firstCiffer = tempMax
-        let rest = list.[firstCiffer.index + 1 .. list.Length]
-        let lastCiffer = rest |> Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
-        firstCiffer.value * 10 + lastCiffer.value
-
+        let rest = l.[firstCiffer.index + 1 .. list.Length]
+        firstCiffer.value * (m |>int64) + getVal (ciffers-1) rest
     let maxJoltageUsingNBatteries filePath n=
         let list = filePath
                 |> loadLinesFromFile
                 |> List.toArray
-                |> Array.map(fun x -> strArr2NumArr x)
+                |> toIntArr
 
-        let toIterItems (l: int array) : iterItem array =
-            l |> Array.mapi(fun i y ->  { index = i; value = y })
 
-        let what =list |> Array.map(fun x -> x |> toIterItems )
-        let a = Array.map(fun x ->  toVal x 2 ) what
-        a |> Array.sum
+        list |> Array.map(fun (x:int64 array)-> getVal n x ) |> Array.sum
+
+// Q2: Answers
+// 7839340789394L - remember to use int64
+// 173416889848394L
 
 
