7289fc329315c3b17b8afb294af1fe2bd55321d1
Theodor Heiselberg <theodor.heiselberg@stil.dk>
2025-12-13 15:41:14 +0100

    Day 03.01 Correct

--- AdventOfCode.Cli/P03.fs
+++ AdventOfCode.Cli/P03.fs
@@ -4,4 +4,4 @@ open AdventOfCode.Lobby
 
 module Lobby =
     let run () =
-        printfn "%A" ( "./AdventOfCode.Test/TestData/day03.01.test.txt" |> answer )
+        printfn "%A" ( "./AdventOfCode.Test/TestData/day03.01.test.txt" |> answer_ )
--- AdventOfCode/Day03.fs
+++ AdventOfCode/Day03.fs
@@ -3,43 +3,36 @@ namespace AdventOfCode
 open AdventOfCode.FileHandler
 module Lobby =
     let strArr2NumArr (strArr: string) : int array = strArr |> Array.ofSeq |> Array.map(fun x -> int x - int '0')
-    let maxOf current next =
-        if current > next then
-            current
-        else
-            next
-    let maxOfExcept (current:int) (next: int) (except:int) : int =
-        if current > next && current <> except then
-            current
-        else
-            next
-    let selectMax arrayMax headMax =
-        if arrayMax > headMax  then headMax else arrayMax
+
     type iterItem = { index: int; value: int }
     let maxWithIndex (prev: iterItem ) (next: iterItem) =
-        if prev.value < next.value then
+        if prev.value >= next.value then
             prev
         else
             next
     let emptyIterItem = { index = -1; value = -1 }
-    let max_ (arr: int array) =
-            arr |> Array.mapi (fun i x-> { index=i; value= x } )
-                |> Array.fold (fun acc x -> maxWithIndex acc x ) emptyIterItem
 
-    let max (arr: int array) =
-        let arrayMax : int = arr |> Array.max
-        let tailValue : int = arr[arr.Length-1]
-        let headMax : int = arr[.. arr.Length - 2] |> Array.max
-        if tailValue = headMax then
-            (headMax * 10) + tailValue
-        else
+    let answer_ filePath =
+        let list = filePath
+                |> loadLinesFromFile
+                |> List.toArray
+                |> Array.map(fun x -> strArr2NumArr x)
 
-        let firstCiffer = selectMax arrayMax headMax
+        let toIterItems (l: int array) : iterItem array =
+            l |> Array.mapi(fun i y ->  { index = i; value = y })
 
-        let lastCiffer : int = arr |> Array.fold (fun acc x-> maxOfExcept acc x firstCiffer) 0
-        firstCiffer * 10 + lastCiffer
-    let answer filePath =
-        filePath |> loadLinesFromFile
-        |> List.toArray
-        |> Array.map (fun x -> strArr2NumArr x)
-        |> Array.map (fun x -> max x)
+        let toVal (l: iterItem array) : int =
+            let lnoTail = l.[0..l.Length-2]
+            let tempMax = lnoTail |>  Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
+            let tail = l.[l.Length-1]
+            if tempMax.value = tail.value then
+                tempMax.value * 10 + tail.value // Return fast!
+            else
+            let firstCiffer = tempMax
+            let rest = l.[firstCiffer.index + 1 .. l.Length]
+            let lastCiffer = rest |> Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
+            firstCiffer.value * 10 + lastCiffer.value
+
+        let what =list |> Array.map(fun x -> x |> toIterItems )
+        let a = Array.map(fun x -> x |> toVal ) what
+        a |> Array.sum
