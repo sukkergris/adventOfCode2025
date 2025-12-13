cf36943beb689f93f33aa9748752aba7f847b58a
Theodor Heiselberg <theodor.heiselberg@stil.dk>
2025-12-13 17:40:28 +0100

    Extract toVal

--- AdventOfCode/Day03.fs
+++ AdventOfCode/Day03.fs
@@ -12,6 +12,20 @@ module Lobby =
             next
     let emptyIterItem = { index = -1; value = -1 }
 
+    let ciffers2Multiplier (ciffers: int) =
+        System.Math.Pow(10.0, float ciffers - 1.0)
+    let rec toVal (list: iterItem array) (ciffers: int): int =
+        let noTailList = list.[0..list.Length-ciffers]
+        let tempMax = noTailList |>  Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
+        let tail = list.[list.Length-1]
+        if tempMax.value = tail.value then
+            tempMax.value * 10 + tail.value // Return fast!
+        else
+        let firstCiffer = tempMax
+        let rest = list.[firstCiffer.index + 1 .. list.Length]
+        let lastCiffer = rest |> Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
+        firstCiffer.value * 10 + lastCiffer.value
+
     let maxJoltageUsingNBatteries filePath n=
         let list = filePath
                 |> loadLinesFromFile
@@ -21,18 +35,8 @@ module Lobby =
         let toIterItems (l: int array) : iterItem array =
             l |> Array.mapi(fun i y ->  { index = i; value = y })
 
-        let toVal (l: iterItem array) : int =
-            let lnoTail = l.[0..l.Length-2]
-            let tempMax = lnoTail |>  Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
-            let tail = l.[l.Length-1]
-            if tempMax.value = tail.value then
-                tempMax.value * 10 + tail.value // Return fast!
-            else
-            let firstCiffer = tempMax
-            let rest = l.[firstCiffer.index + 1 .. l.Length]
-            let lastCiffer = rest |> Array.fold(fun prev next -> maxWithIndex prev next) emptyIterItem
-            firstCiffer.value * 10 + lastCiffer.value
-
         let what =list |> Array.map(fun x -> x |> toIterItems )
-        let a = Array.map(fun x -> x |> toVal ) what
+        let a = Array.map(fun x ->  toVal x 2 ) what
         a |> Array.sum
+
+
