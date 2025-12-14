b8cd230e6bcddac6d1a5180ca55660c807991f18
Theodor Heiselberg <theodor.heiselberg@stil.dk>
2025-12-14 08:17:59 +0100

    Pick from board

--- AdventOfCode/Day04.fs
+++ AdventOfCode/Day04.fs
@@ -18,6 +18,9 @@ module PrintingDepartment =
             toCoord (p.x-1) p.y; toCoord (p.x+1) p.y ;
             toCoord p.x (p.y+1)] |> List.filter(fun x -> x.x > -1 && x.x < abscissa && x.y > - 1 && x.y < ordinate)
 
+    let tryFind (board: point list list) (coord:coord) =
+        board |> List.map(fun x -> x |> List.try(fun p -> p.x =coord.x && p.y = coord.y )) |>
+
     let canLiftStatus (p: point) (abscissa: int) (ordinate:int) =
         let a = getAdjacentCoords p abscissa ordinate
                     // |> List.filter (fun x -> x.)
